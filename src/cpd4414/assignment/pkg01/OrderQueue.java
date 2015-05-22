95616e7ccc56787dd44dedce5cad8b65f57e72f0
c0653602 <c0653602@SB-N208-28.lambton.on.ca>
2015-05-22 09:52:55 -0400

    tushar

--- src/cpd4414/assignment/pkg01/OrderQueue.java
+++ src/cpd4414/assignment/pkg01/OrderQueue.java
@@ -6,10 +6,34 @@
 
 package cpd4414.assignment.pkg01;
 
+import java.util.ArrayList;
+import java.util.ArrayDeque;
+import java.util.List;
+import java.util.Date;
+import java.util.Queue;
+
+
+
 /**
  *
  * @author c0652863
  */
 public class OrderQueue {
+    Queue<Order> orderque = new ArrayDeque();
+    List<Order> orderList = new ArrayList();
     
+    public void add(Order order) throws CustException {
+        if(order.getCustomerId().isEmpty() && order.getCustomerName().isEmpty()){
+        throws new CustException();
+    }
+
+    public class CustException extends Exception {
+
+        public CustException() {
+            super("No Customer Provided.");
+        }
+    }
+    
+    
+
 }
