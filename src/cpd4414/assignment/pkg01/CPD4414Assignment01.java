d161db6aa46ebe689d8c0d2d7f51608f6a53a388
c0652512 <c0652512@SB-N208-22.lambton.on.ca>
2015-05-22 09:00:29 -0400

    / hardik made some changes

--- src/cpd4414/assignment/pkg01/CPD4414Assignment01.java
+++ src/cpd4414/assignment/pkg01/CPD4414Assignment01.java
@@ -18,7 +18,8 @@ public class CPD4414Assignment01 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("hello");
+        
+         System.out.println("hardik here..");
     }
     
 }
