da519c5c373b494cc8af789228982fb5974e8ca6
c0654504 <c0654504@SB-N208-27.lambton.on.ca>
2015-05-22 09:20:54 -0400

    jay

--- src/cpd4414/assignment/pkg01/Purchase.java
+++ src/cpd4414/assignment/pkg01/Purchase.java
@@ -11,5 +11,27 @@ package cpd4414.assignment.pkg01;
  * @author c0652863
  */
 public class Purchase {
-    
+    private String productID;
+    private int quantity;
+
+    public Purchase(String productID, int quantity) {
+        this.productID = productID;
+        this.quantity = quantity;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public void setProductID(String productID) {
+        this.productID = productID;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
