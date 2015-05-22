d9501807342f543194a20ade7c873100cb4e7e32
c0652512 <c0652512@SB-N208-22.lambton.on.ca>
2015-05-22 09:28:29 -0400

    / hardik made some changes

--- src/cpd4414/assignment/pkg01/Order.java
+++ src/cpd4414/assignment/pkg01/Order.java
@@ -6,10 +6,95 @@
 
 package cpd4414.assignment.pkg01;
 
+import java.util.Date;
+
 /**
  *
  * @author c0652863
  */
 public class Order {
+    
+    // Declaring all the variables
+ 
+    
+    public Date timeReceived;
+   public String customerId;
+    public String customerName;
+    public Date timeProcessed;
+    public Date timeFulfilled;
+    public String customerNotes;
+    
+    
+    
+    
+    //----------------------------------------------------------------
+    // Constructor
+
+    public Order(Date timeReceived, String customerId, String customerName, Date timeProcessed, Date timeFulfilled, String customerNotes) {
+        this.timeReceived = timeReceived;
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.timeProcessed = timeProcessed;
+        this.timeFulfilled = timeFulfilled;
+        this.customerNotes = customerNotes;
+    }
+     
+
+//---------------------------------------------------------------------------
+    
+    //Get Set Methods
+
+    public Date getTimeReceived() {
+        return timeReceived;
+    }
+
+    public void setTimeReceived(Date timeReceived) {
+        this.timeReceived = timeReceived;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public Date getTimeProcessed() {
+        return timeProcessed;
+    }
+
+    public void setTimeProcessed(Date timeProcessed) {
+        this.timeProcessed = timeProcessed;
+    }
+
+    public Date getTimeFulfilled() {
+        return timeFulfilled;
+    }
+
+    public void setTimeFulfilled(Date timeFulfilled) {
+        this.timeFulfilled = timeFulfilled;
+    }
+
+    public String getCustomerNotes() {
+        return customerNotes;
+    }
+
+    public void setCustomerNotes(String customerNotes) {
+        this.customerNotes = customerNotes;
+    }
+    
+  //---------------------------------------------------------------
+    
+    
+    
     
 }
