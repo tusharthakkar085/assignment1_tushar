4ae3b7c2d36d01ebbb202d181102dfffcfcf9527
c0652863 <c0652863@SB-N208-26.lambton.on.ca>
2015-05-22 09:15:10 -0400

    rama

---
+++ src/cpd4414/assignment/pkg01/OrderQueue.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cpd4414.assignment.pkg01;
+
+/**
+ *
+ * @author c0652863
+ */
+public class OrderQueue {
+    
+}
